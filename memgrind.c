d9eba9f76d55c3c7562fc551f6ce59aaa1f7f53b
jtIOO <99695676+jtIOO@users.noreply.github.com>
2022-02-14 23:21:08 -0500

    Update memgrind.c

--- memgrind.c
+++ memgrind.c
@@ -3,6 +3,8 @@
 //#include "mymalloc.h"
 
 char *arr[120];// an array of pointers
+int test2Time[50];
+int time2; 
 
 
 void test1(){ //allocate then free 120 times //ill do this one
@@ -18,9 +20,13 @@ printf("memgrinded: %d\n", i+1);
 
 
 void test2(){ //allocate 120 times then free //you got this one
+    struct timeval start;
+    struct timeval end;
     
     int i;
+
     for(i = 0; i< 120;i++){
+        gettimeofday(&start,NULL);
         arr[i] =(char*)malloc(1);
         printf("allocated: %p, size: 1", arr[i]);
     }
@@ -28,6 +34,9 @@ void test2(){ //allocate 120 times then free //you got this one
         free(arr[i]);
         printf("free\n");
     }
+    gettimeofday(&end,NULL);
+    time2 += end.tv_sec-start.tv_sec;
+    
     
 
 }
@@ -45,6 +54,7 @@ void test5(){ //i think it would be calm to allocate memory in 20 part bits and 
 }
 
 int main (int argc, char **argv){ //get the time per test all that is missing
+    int sum2 = 0;
     for (int i = 0; i<50; i++){
         test1();
         test2();
@@ -52,7 +62,13 @@ int main (int argc, char **argv){ //get the time per test all that is missing
         test4();
         test5();
         printf("test time: %d\n", i+1);
+        test2Time[i] = time2;
     }
+    int j;
+    for(j=0;j<50;j++){
+        sum2+=test2Time[i];
+    }
+    printf("average time for test 2: %d\n", sum2/50);
     
     return 0;
 }
