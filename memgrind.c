b1ad2f9c38b8922ad0016d087ff626d87eca53a8
jtIOO <cl1309@rutgers.edu>
2022-02-28 20:53:07 -0500

    Test1 -3

--- memgrind.c
+++ memgrind.c
@@ -1,52 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mymalloc.h"
-
-char *arr[120];// an array of pointers
-int test2Time[50];
-int time2; 
-
-void test0(){
-    char *p = (char*)malloc(sizeof(char)); //
-    printf("allocated: %p, size: %lu\n", p, sizeof(p));
-}
+#include <time.h>
+#include <sys/time.h>
+char* arr[120];
 
 
-/* void test1(){ //allocate then free 120 times //ill do this one
-for(int i = 0;i<120;i++){//loop through 120 times
-char *p = (char*)malloc(sizeof(char)); //
-printf("allocated: %p, size: %lu\n", p, sizeof(p));
-free(p);
-printf("freed\n");
-printf("memgrinded: %d\n", i+1);
+double total1Time = 0;
+double total2Time = 0;
+double total3Time = 0;
+
+
+struct timeval start, end;
+
+
+
+
+void test1(){ //allocate then free 120 times //ill do this one
+    for(int i = 0; i<50; i++){
+        initiate();
+        gettimeofday(&start,NULL);
+        for(int j = 0;j<120;j++){//loop through 120 times
+            char *p = (char*)malloc(sizeof(char)); //
+            free(p);
+        gettimeofday(&end, NULL);
+        total1Time +=((end.tv_usec-start.tv_usec)/1000000.0);
+        printf("time :%f\n", total1Time);
+    }
 }
 
 }//end test 1
 
 
 void test2(){ //allocate 120 times then free //you got this one
-    struct timeval start;
-    struct timeval end;
+int i, j, k;
+for(i = 0;i<50;i++){
     
-    int i;
+    initiate();
+    gettimeofday(&start,NULL);
+    for(j = 0; j< 120;j++){
 
-    for(i = 0; i< 120;i++){
-        gettimeofday(&start,NULL);
         arr[i] =(char*)malloc(1);
-        printf("allocated: %p, size: 1", arr[i]);
     }
-    for(i = 0; i<120; i++){
+    for(k = 0; k<120; k++){
         free(arr[i]);
-        printf("free\n");
     }
     gettimeofday(&end,NULL);
-    time2 += end.tv_sec-start.tv_sec;
-    
-    
+    total2Time +=((end.tv_usec-start.tv_usec)/1000000.0);
+    printf("time :%f\n",total2Time);
+} 
 
 }
 
 void test3(){//random allocate, random free, dont free null
+    for(int i = 0; i<50; i++){
+        initiate();
+        gettimeofday(&start, NULL);
+        char* arr1[120];
+        int countFree = 0;
+        int countMalloc = 0;
+        int randomNum = 0;
+        while(countMalloc <120){
+            randomNum = rand()%2;
+            if(randomNum == 0){
+                arr1[countMalloc] =malloc(1);
+                countMalloc++;
+
+            }
+            else{
+                if(countMalloc>countFree){
+                    free(arr1[countFree]);
+                    countFree++;
+                }
+            }
+
+        }
+        while(countFree!=countMalloc){
+            free(arr1[countFree]);
+            countFree++;
+        }
+        gettimeofday(&end,NULL);
+        total3Time+= ((end.tv_usec-start.tv_usec)/1000000.0);
+        printf("time :%f\n",total3Time);
+    }
 
 }
 
@@ -57,9 +93,14 @@ void test4(){ //some bullshit // you plan this one out
 void test5(){ //i think it would be calm to allocate memory in 20 part bits and then deallocate those 20 part bits similar to thee tests before just a tad different lmao
 
 }
- */
 int main (int argc, char **argv){ //get the time per test all that is missing
-    test0();
+    test1();
+   printf("the average time of test1 is: %f\n", total1Time/50);
+    test2();
+    printf("the average time of test2 is: %f\n", total2Time/50);
+    test3();
+    printf("the average time of test3 is: %f\n", total3Time/50);
+
     
     return 0;
 }
